d241f8557a395a89cc75493fe1abadeac889d774
LineYK <yhoung11@gmail.com>
2026-07-28 02:17:51 +0900

    MainScene 및 관련 리소스 추가

---
+++ Assets/Scripts/CamRotate.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class CamRotate : MonoBehaviour
+{
+
+    public float rotSpeed = 200f;
+
+    float mx = 0;
+    float my = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouse_X = Input.GetAxis("Mouse X");
+        float mouse_Y = Input.GetAxis("Mouse Y");
+
+        Vector3 dir = new Vector3(-mouse_Y, mouse_X, 0);
+
+        transform.eulerAngles += dir * rotSpeed * Time.deltaTime;
+
+        // x축 회전 값을 -90도 ~ 90도 사이로 제한
+        Vector3 rot = transform.eulerAngles;
+        rot.x = Mathf.Clamp(rot.x, -90f, 90f);
+        transform.eulerAngles = rot;
+    }
+}
