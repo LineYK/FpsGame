b85d1f9332fc7fbc3c3769239f632dbfd83a6265
LineYK <yhoung11@gmail.com>
2026-08-02 02:43:57 +0900

    점프/이동 로직 업데이트

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -3,14 +3,20 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public float moveSpeed = 7f;
+    
+    CharacterController cc;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    float gravity = -20f;
+    float yVelocity = 0;
+
+    public float jumpPower = 10f;
+    public bool isJumping = false;
+
     void Start()
     {
-        
+        cc = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         float h = Input.GetAxis("Horizontal");
@@ -18,7 +24,27 @@ public class PlayerMove : MonoBehaviour
 
         Vector3 dir = new Vector3(h, 0, v);
         dir = dir.normalized;
+        
+        dir = Camera.main.transform.TransformDirection(dir);
 
-        transform.position += dir * moveSpeed * Time.deltaTime;
+        if (cc.collisionFlags == CollisionFlags.Below)
+        {
+            if (isJumping) 
+                isJumping = false;
+            
+            yVelocity = 0;    
+        }
+
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            yVelocity = jumpPower;
+            isJumping = true;
+        }
+
+        yVelocity += gravity * Time.deltaTime;
+        dir.y = yVelocity;
+
+        cc.Move(dir * moveSpeed * Time.deltaTime);
+
     }
 }
