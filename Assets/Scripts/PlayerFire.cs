66cdce85fb692069cebbaa09113fa899480262dd
LineYK <yhoung11@gmail.com>
2026-08-21 01:32:42 +0900

    무기 모드 추가 및 UI 텍스트 업데이트

--- Assets/Scripts/PlayerFire.cs
+++ Assets/Scripts/PlayerFire.cs
@@ -1,7 +1,15 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class PlayerFire : MonoBehaviour
 {
+    private enum WeaponMode
+    {
+        Normal,
+        Sniper
+    }
+
     public GameObject firePostion;
 
     public GameObject bombFactory;
@@ -18,11 +26,20 @@ public class PlayerFire : MonoBehaviour
 
     private Animator anim;
 
+    private WeaponMode wMode;
+
+    private bool ZoomMode = false;
+
+    [SerializeField]
+    private TMP_Text wModeText;
+
     void Start()
     {
         ps = bulletEffect.GetComponent<ParticleSystem>();
 
         anim = GetComponentInChildren<Animator>();
+
+        wMode = WeaponMode.Normal;
     }
 
     void Update()
@@ -32,14 +49,35 @@ public class PlayerFire : MonoBehaviour
             return;
         }
 
+        /*
+         * 노멀 모드: 마우스 오른쪽 버튼을 누르면 시선방향으로 수류탄
+         * 스나이퍼 모드: 마우스 오른쪽 버튼을 누르면 화면을 확대
+         */
         if (Input.GetMouseButtonDown(1))
         {
-            GameObject bomb = Instantiate(bombFactory);
-            bomb.transform.position = firePostion.transform.position;
+            switch (wMode)
+            {
+                case WeaponMode.Normal:
+                    GameObject bomb = Instantiate(bombFactory);
+                    bomb.transform.position = firePostion.transform.position;
 
-            Rigidbody rb = bomb.GetComponent<Rigidbody>();
+                    Rigidbody rb = bomb.GetComponent<Rigidbody>();
 
-            rb.AddForce(Camera.main.transform.forward *  throwPower, ForceMode.Impulse);
+                    rb.AddForce(Camera.main.transform.forward * throwPower, ForceMode.Impulse);
+                    break;
+                case WeaponMode.Sniper:
+                    if(!ZoomMode)
+                    {
+                        Camera.main.fieldOfView = 15f;
+                        ZoomMode = true;
+                    } 
+                    else
+                    {
+                        Camera.main.fieldOfView = 60f;
+                        ZoomMode = false;
+                    }
+                    break;
+            }
         }
 
         if (Input.GetMouseButtonDown(0))
@@ -62,12 +100,31 @@ public class PlayerFire : MonoBehaviour
                 else
                 {
                     bulletEffect.transform.position = hitInfo.point;
-                    // 피격 이펙트의 forward 방향을 레이가 부딪힌 지점의 범선 벡터와 일치
+                    // 피격 이펙트의 forward 방향을 레이가 부딪힌 지점의 법선 벡터와 일치
                     bulletEffect.transform.forward = hitInfo.normal;
 
                     ps.Play();
                 }
             }
         }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            wMode = WeaponMode.Normal;
+
+            // 카메라 시야각 복구
+            Camera.main.fieldOfView = 60f;
+
+            wModeText.text = "Normal Mode";
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            wMode = WeaponMode.Sniper;
+
+            wModeText.text = "Sniper Mode";
+        }
+
     }
+
+
 }
